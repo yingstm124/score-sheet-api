c7b54c2260e6694a2beae156136a2ff38a66d25d
yingstm124 <yingstm124@gmail.com>
2021-09-06 02:18:05 +0700

    send srudent assignment id

--- score_sheet_api/controllers/ImageController.py
+++ score_sheet_api/controllers/ImageController.py
@@ -33,12 +33,20 @@ def uploadImage():
     if(request.method == "POST"):
 
         try:
-            student_assignment_id = request.args.get('studentAssignmentId')
+            teach_std_id = request.args.get('TeachStudentId')
+            assignment_id = request.args.get('AssignmentId')
             if(request.files['image']):
                 image = request.files['image']
                 filename = werkzeug.utils.secure_filename(image.filename)
 
-                query_select = "SELECT S.img FROM studentassignments S  WHERE StudentAssignmentId={0}".format(student_assignment_id)
+                # Find Student Assignment Id
+                query_select_id = "select SA.StudentAssignmentId from studentassignments SA Where TeachStudentId = {0} AND AssignmentId = {1}".format(teach_std_id,assignment_id)
+                cursor.execute(query_select_id)
+                res = cursor.fetchone()
+                student_assign_id = res["StudentAssignmentId"]
+
+                # Check duplicate image
+                query_select = "SELECT S.img FROM studentassignments S  WHERE StudentAssignmentId={0}".format(student_assign_id)
                 cursor.execute(query_select)
                 res = cursor.fetchone()
 
@@ -46,22 +54,23 @@ def uploadImage():
                     os.remove(app.root_path+res['img'])
 
                 query = "UPDATE studentassignments SET img=%s WHERE StudentAssignmentId=%s"
-                cursor.execute(query,('/static/'+filename, int(student_assignment_id)))
+                cursor.execute(query,('/static/'+filename, int(student_assign_id)))
+                
 
                 print(app.root_path)
                 pathImage = os.path.join(app.root_path,'./static/',filename)
                 print(pathImage)
                 image.save(pathImage)
             
-                return jsonify(True), 200
+                return jsonify(student_assign_id), 200
 
             else:
-                return jsonify(True), 204
+                return jsonify(0), 204
             
         
         except Exception as err:
             print(err)
-            return Handle_error(False)
+            return Handle_error(0, 500)
              
 
         
