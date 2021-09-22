b9ad1aed249c9c2cfd1ea7cca41774327880159f
yingstm124 <yingstm124@gmail.com>
2021-09-22 23:59:31 +0700

    add host db

--- score_sheet_api/config/database.py
+++ score_sheet_api/config/database.py
@@ -1,14 +1,14 @@
 
 import pymysql
-import pymssql
+import pyodbc
 
 def getDb():
-    
+
     # conn = pymysql.connect(
 
     #     user = 'root',
     #     host = 'localhost', 
-    #     password = '1234567890', 
+    #     password = '27365410', 
     #     database = 'ScoreSheet', 
     #     autocommit = True, 
     #     charset = 'utf8mb4', 
@@ -17,14 +17,15 @@ def getDb():
 
     conn = pymysql.connect(
 
-        user = 'root',
-        host = 'localhost', 
-        password = '27365410', 
-        database = 'ScoreSheet', 
+        user = 'b6308f2376f3a0',
+        host = 'us-cdbr-east-04.cleardb.com', 
+        password = 'fd4624b8', 
+        database = 'heroku_27ddfa27b25fbc7', 
         autocommit = True, 
         charset = 'utf8mb4', 
         cursorclass = pymysql.cursors.DictCursor
     )
+
 
     return conn
 
