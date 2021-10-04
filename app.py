fddb4281ed9684e3ed62f25a596a4ca1cdda5079
yingstm124 <yingstm124@gmail.com>
2021-10-04 09:25:44 +0700

    update app run

--- app.py
+++ app.py
@@ -2,4 +2,5 @@ import os
 from score_sheet_api import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0",port=8080) 
+    app.run() 
+    # app.run(debug=True, host="0.0.0.0",port=8080) 
