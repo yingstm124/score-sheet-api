ee4862dbe7fb9c0b3f7a224551eeb25753681773
Meehoi <beam_baimon@hotmail.com>
2021-10-04 22:32:36 +0700

    add file

--- app.py
+++
@@ -1,7 +0,0 @@
-import os
-from score_sheet_api import app
-import socket
-
-if __name__ == '__main__':
-    app.run(debug=True, host=socket.gethostbyname(socket.gethostname())) 
-    # app.run(debug=True, host="0.0.0.0",port=8080) 
