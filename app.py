c617f16ba35190a7628c987132cd1b31c876e0b4
yingstm124 <yingstm124@gmail.com>
2021-10-14 01:10:45 +0700

    update app run

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from score_sheet_api import app
 import socket
 
 if __name__ == '__main__':
-    app.run(debug=True, host=socket.gethostbyname(socket.gethostname())) 
-    # app.run(debug=True, host="0.0.0.0",port=8080) 
+    # app.run(debug=True, host=socket.gethostbyname(socket.gethostname())) 
+    app.run(host="0.0.0.0",port=5000) 
